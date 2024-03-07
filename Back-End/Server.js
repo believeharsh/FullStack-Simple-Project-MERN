b7d15e42cd1ed0754bc20e06418eab6f1d2c9988
Believeharsh <bontinue@gmail.com>
2024-03-07 19:10:11 +0530

    understand few concept of Back-End of MongoDB, Node

--- Back-End/Server.js
+++ Back-End/Server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
+app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
 const userData = require("./Models/UserModel");
@@ -21,6 +23,68 @@ app.get("/", (req, res) => {
   res.send("api running");
 });
 
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const { name, email, age } = req.body;
+  try {
+    const userAdded = await userData.create({
+      name: name,
+      email: email,
+      age: age,
+    });
+    res.status(201).json(userAdded);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const allUsers = await userData.find();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//GET SINGLE USER
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const singleUser = await userData.findById({ _id: id });
+    res.status(200).json(singleUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+outer.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await userData.findByIdAndDelete({ _id: id });
+    res.status(201).json(deletedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//UPDATE
+router.patch("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log("get body", req.body);
+  console.log("get id", id);
+  //const { name, email, age } = req.body;
+
+  try {
+    const updatedUser = await userData.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 
 
+  app.use(userDataRoute);
 
